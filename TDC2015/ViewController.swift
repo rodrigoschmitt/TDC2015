7a67d7c0b7c0b32e6e13c12c91fecc65b06531ca
Rodrigo Andrade <rodrigo.andrade@me.com>
2015-05-14 10:39:06 -0300

    Initial Commit

---
+++ TDC2015/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDC2015
+//
+//  Created by Rodrigo S Andrade on 5/14/15.
+//  Copyright (c) 2015 DevMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
